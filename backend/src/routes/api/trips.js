1c372a0b7217dc4351389b763be30ac4f28482a0
Pranay Jhaveri <pjha607@aucklanduni.ac.nz>
2021-04-07 22:13:14 +1200

    Added api endpoint for trips

---
+++ backend/src/routes/api/trips.js
@@ -0,0 +1,11 @@
+import express from "express";
+
+const HTTP_CREATED = 201;
+const HTTP_NOT_FOUND = 404;
+const HTTP_NO_CONTENT = 204;
+
+const router = express.Router();
+
+// TODO: Need to add API endpoints
+
+export default router;

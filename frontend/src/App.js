6d6e0e3c4de41bd0056334c239e31f5ece65fab4
Pranay Jhaveri <pjha607@aucklanduni.ac.nz>
2021-04-20 18:52:53 +1200

    Created sign up functionality of app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,8 @@ import { useState } from "react";
 import * as Realm from "realm-web";
 import LoginPage from "./components/sign-in/LoginPage.jsx";
 import UserDetail from "./components/sign-in/UserDetail.jsx";
+import SignUp from "./components/sign-in/SignUp.jsx";
+import { Switch } from "@material-ui/core";
 
 // Realm app to authenticate user
 const REALM_APP_ID = "are-we-there-yet-zaqns";
@@ -9,11 +11,20 @@ export const app = new Realm.App({ id: REALM_APP_ID });
 
 function App() {
   const [user, setUser] = useState(app.currentUser);
+  const [checked, setChecked] = useState(false);
 
   return (
     <div>
+      <Switch
+        checked={checked}
+        onChange={() => setChecked(!checked)}
+        name="checkA"
+        inputProps={{ "aria-label": "primary checkbox" }}
+      />
       {user ? (
         <UserDetail user={user} setUser={setUser} />
+      ) : checked ? (
+        <SignUp />
       ) : (
         <LoginPage setUser={setUser} />
       )}
---
+++ frontend/src/components/sign-in/SignUp.jsx
@@ -0,0 +1,89 @@
+import React, { useState } from "react";
+import { Formik } from "formik";
+import { TextField, Button } from "@material-ui/core";
+import { app } from "../../App";
+
+// Helper function to determine if email and password are correct
+const userRegistration = async (
+  email,
+  password,
+  setEmailError,
+  setPasswordError
+) => {
+  if (
+    email.length < 6 ||
+    email.length > 128 ||
+    password.length < 6 ||
+    password.length > 128
+  ) {
+    setEmailError(true);
+    setPasswordError(true);
+    return;
+  }
+
+  try {
+    await app.emailPasswordAuth.registerUser(email, password);
+    setEmailError(false);
+    setPasswordError(false);
+  } catch {
+    setEmailError(true);
+    setPasswordError(true);
+  }
+};
+
+const SignUp = () => {
+  const [emailError, setEmailError] = useState(false);
+  const [passwordError, setPasswordError] = useState(false);
+
+  return (
+    <div>
+      <h1>Sign Up</h1>
+      <Formik
+        initialValues={{ email: "", password: "" }}
+        onSubmit={(values, actions) => {
+          //   app.emailPasswordAuth.registerUser(values.email, values.password);
+          userRegistration(
+            values.email,
+            values.password,
+            setEmailError,
+            setPasswordError
+          );
+          actions.setSubmitting(false);
+        }}
+      >
+        {(props) => (
+          <form onSubmit={props.handleSubmit}>
+            <TextField
+              label="Email"
+              onChange={props.handleChange}
+              onBlur={props.handleBlur}
+              value={props.values.email}
+              id="email"
+              variant="outlined"
+              error={emailError}
+            />
+            <TextField
+              label="Password"
+              type="password"
+              onChange={props.handleChange}
+              onBlur={props.handleBlur}
+              value={props.values.password}
+              id="password"
+              variant="outlined"
+              error={passwordError}
+            />
+            <Button
+              type="submit"
+              style={{ backgroundColor: "#1a73e8", color: "#fff" }}
+              variant="contained"
+            >
+              Sign Up
+            </Button>
+          </form>
+        )}
+      </Formik>
+    </div>
+  );
+};
+
+export default SignUp;

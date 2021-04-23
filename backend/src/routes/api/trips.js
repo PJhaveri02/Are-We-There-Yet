2c4b2940982408047a276621f6ac66ea0cb6fa5d
Pranay Jhaveri <pjha607@aucklanduni.ac.nz>
2021-04-23 18:49:49 +1200

    Added HTTP Bad Request status code.

--- backend/src/routes/api/trips.js
+++ backend/src/routes/api/trips.js
@@ -12,6 +12,7 @@ const HTTP_CREATED = 201;
 const HTTP_NOT_FOUND = 404;
 const HTTP_NO_CONTENT = 204;
 const HTTP_UNAUTHORISED = 401;
+const HTTP_BAD_REQUEST = 400;
 
 const router = express.Router();
 

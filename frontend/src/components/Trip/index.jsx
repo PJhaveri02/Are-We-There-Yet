fe6f1a3aae2bcdfbc19ccfd11c1cbcc3ce993a8a
Daniel Yeom <dyeo713@aucklanduni.ac.nz>
2021-04-27 18:22:19 +1200

    updated with Develop branch

--- frontend/src/components/Trip/index.jsx
+++ frontend/src/components/Trip/index.jsx
@@ -1,25 +1,20 @@
 import React, { useState } from "react";
-import { useLoadScript } from "@react-google-maps/api";
 import Searchbar from "./Searchbar";
 import DestinationList from "./DestinationList";
 
 function Trip() {
-  const libraries = ["places"];
-
-  const { isLoaded, loadError } = useLoadScript({
-    googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAPS_API_KEY,
-    libraries,
-  });
-
   const [destinations, setDestinations] = useState([]);
-
-  if (loadError) return "Error loading maps";
-  if (!isLoaded) return "Loading...";
 
   const onDestinationSelect = (destination, lat, lng) => {
     setDestinations([
       ...destinations,
-      { startDate: "2021-04-27", locationName: destination, timeSpent: 1 , lat: lat, lng: lng},
+      {
+        startDate: "2021-04-27",
+        locationName: destination,
+        timeSpent: 1,
+        lat: lat,
+        lng: lng,
+      },
     ]);
     console.log(destinations);
   };

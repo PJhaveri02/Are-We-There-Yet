9af09e88ad8ffa077f9e230a676ee4d91d2d0565
Pranay Jhaveri <pjha607@aucklanduni.ac.nz>
2021-04-30 22:39:08 +1200

    Styled sign-in page and added google places dependency

--- frontend/src/components/sign-in/LoginPage.jsx
+++ frontend/src/components/sign-in/LoginPage.jsx
@@ -1,4 +1,15 @@
-import { Button, TextField } from '@material-ui/core';
+import {
+  Button,
+  Checkbox,
+  Container,
+  CssBaseline,
+  FormControlLabel,
+  Grid,
+  Link,
+  makeStyles,
+  TextField,
+  Typography,
+} from '@material-ui/core';
 import { Formik } from 'formik';
 import React, { useContext } from 'react';
 import { useHistory } from 'react-router';
@@ -14,61 +25,107 @@ const login = async (setUser, email, password, history) => {
     }
     setUser(user);
   } catch (err) {
-    console.log('Invalid username/password (status 401)');
+    alert('Invalid username/password');
   }
 };
 
-const handleRoutingToSignUpPage = (history) => {
-  history.push('/sign-up');
-};
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  form: {
+    width: '100%',
+    marginTop: theme.spacing(1),
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.secondary.main,
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+  },
+}));
 
 const LoginPage = () => {
   const [, setUser] = useContext(AuthContext);
   const history = useHistory();
+  const classes = useStyles();
 
   return (
-    <div>
-      <h1>Login Page</h1>
-      <Formik
-        initialValues={{ email: '', password: '' }}
-        onSubmit={(values, actions) => {
-          login(setUser, values.email, values.password, history);
-          actions.setSubmitting(false);
-        }}
-      >
-        {(props) => (
-          <form onSubmit={props.handleSubmit}>
-            <TextField
-              label='Email'
-              onChange={props.handleChange}
-              onBlur={props.handleBlur}
-              value={props.values.email}
-              id='email'
-              variant='outlined'
-            />
-            <TextField
-              label='Password'
-              type='password'
-              onChange={props.handleChange}
-              onBlur={props.handleBlur}
-              value={props.values.password}
-              id='password'
-              variant='outlined'
-            />
-            <Button
-              type='submit'
-              style={{ backgroundColor: '#1a73e8', color: '#fff' }}
-              variant='contained'
-            >
-              Login
-            </Button>
-            <Button variant='contained' onClick={() => handleRoutingToSignUpPage(history)}>
-              Sign Up
-            </Button>
-          </form>
-        )}
-      </Formik>
-    </div>
+    <Container component='main' maxWidth='xs'>
+      <CssBaseline />
+      <div className={classes.paper}>
+        <Typography component='h1' variant='h5'>
+          Sign In
+        </Typography>
+        <Formik
+          initialValues={{ email: '', password: '' }}
+          onSubmit={(values, actions) => {
+            login(setUser, values.email, values.password, history);
+            actions.setSubmitting(false);
+          }}
+        >
+          {(props) => (
+            <form onSubmit={props.handleSubmit} className={classes.form}>
+              <TextField
+                label='Email'
+                onChange={props.handleChange}
+                onBlur={props.handleBlur}
+                value={props.values.email}
+                id='email'
+                variant='outlined'
+                margin='normal'
+                fullWidth
+                autoFocus
+              />
+              <TextField
+                label='Password'
+                type='password'
+                onChange={props.handleChange}
+                onBlur={props.handleBlur}
+                value={props.values.password}
+                id='password'
+                variant='outlined'
+                margin='normal'
+                fullWidth
+                autoFocus
+              />
+              <FormControlLabel
+                control={<Checkbox value='remember' color='primary' />}
+                label='Remember me'
+              />
+              <Button
+                type='submit'
+                style={{ backgroundColor: '#1a73e8', color: '#fff' }}
+                variant='contained'
+                fullWidth
+                className={classes.submit}
+              >
+                Login
+              </Button>
+              <Grid container>
+                <Grid item xs>
+                  <Link href='#' variant='body2'>
+                    Forgot password?
+                  </Link>
+                </Grid>
+                <Grid item>
+                  <Link
+                    variant='body2'
+                    onClick={() => history.push('/sign-up')}
+                    style={{ cursor: 'pointer' }}
+                  >
+                    {"Don't have an account? Sign Up"}
+                  </Link>
+                </Grid>
+              </Grid>
+            </form>
+          )}
+        </Formik>
+      </div>
+    </Container>
   );
 };
 

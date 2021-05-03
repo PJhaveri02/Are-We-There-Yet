1addd8587c3566750ff9b6d7c893ca56ea411bc2
Daniel Yeom <dyeo713@aucklanduni.ac.nz>
2021-05-03 16:43:24 +1200

    restructured files

--- frontend/src/components/TripList/NewTripModal/index.jsx
+++ frontend/src/components/TripList/NewTripModal/index.jsx
@@ -1,3 +1,31 @@
-import React from "react";
+import React, { useState } from 'react';
+import Searchbar from './Searchbar';
+import DestinationList from './DestinationList';
 
-// TODO implement functionality to add new trips
+function SearchBar() {
+  const [destinations, setDestinations] = useState([]);
+
+  const onDestinationSelect = (destination, lat, lng) => {
+    setDestinations([
+      ...destinations,
+      {
+        startDate: '2021-04-27',
+        locationName: destination,
+        timeSpent: 1,
+        lat: lat,
+        lng: lng,
+      },
+    ]);
+    console.log(destinations);
+  };
+
+  return (
+    <div>
+      {/* name, start date */}
+      <DestinationList destinations={destinations} />
+      <Searchbar onDestinationSelect={onDestinationSelect} />
+    </div>
+  );
+}
+
+export default SearchBar;

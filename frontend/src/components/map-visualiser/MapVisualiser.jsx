f497cc3f106dc4666d92746e1ed6526e5872a1e9
Pranay Jhaveri <pjha607@aucklanduni.ac.nz>
2021-05-05 19:17:47 +1200

    Moved slider to sidebar

--- frontend/src/components/map-visualiser/MapVisualiser.jsx
+++ frontend/src/components/map-visualiser/MapVisualiser.jsx
@@ -1,33 +1,21 @@
-import React, { useContext, useState } from 'react';
+import React, { useContext } from 'react';
 import { ResourceContext } from '../../pages/Homepage';
-import { defaultCenter } from './DefaultSettings';
 import Map from './Map';
-import Slider from './Slider';
 
-export default function MapVisualizer() {
+export default function MapVisualizer({ center, setCenter, sliderPosition, setSliderPosition }) {
   // need these two states for map to display location based on slider
   const { trip } = useContext(ResourceContext);
-  const [sliderPosition, setSliderPosition] = useState(1);
-  const [center, setCenter] = useState(defaultCenter);
 
   return (
     <div>
       {trip ? (
-        <>
-          <Map
-            origin={trip.stops[0]}
-            destination={trip.stops[trip.stops.length - 1]}
-            stops={trip.stops}
-            sliderPosition={sliderPosition}
-            center={center}
-          />
-          <Slider
-            step={1}
-            stops={trip.stops}
-            onChangeSlider={setSliderPosition}
-            setCenter={setCenter}
-          />
-        </>
+        <Map
+          origin={trip.stops[0]}
+          destination={trip.stops[trip.stops.length - 1]}
+          stops={trip.stops}
+          sliderPosition={sliderPosition}
+          center={center}
+        />
       ) : (
         <Map center={center} />
       )}
--- frontend/src/pages/Homepage.jsx
+++ frontend/src/pages/Homepage.jsx
@@ -1,9 +1,11 @@
-import React, { createContext } from 'react';
+import React, { createContext, useState } from 'react';
 import MapVisualizer from '../components/map-visualiser/MapVisualiser';
 import TripList from '../components/TripList';
 import styles from './Homepage.module.css';
 import LogoutButton from '../components/signing-pages/LogoutButton';
 import useResources from '../hooks/useResources';
+import { defaultCenter } from '../components/map-visualiser/DefaultSettings';
+import Slider from '../components/map-visualiser/Slider';
 
 export const ResourceContext = createContext(undefined);
 
@@ -18,6 +20,8 @@ function Homepage() {
     version,
     setVersion,
   } = useResources();
+  const [sliderPosition, setSliderPosition] = useState(1);
+  const [center, setCenter] = useState(defaultCenter);
 
   return (
     <ResourceContext.Provider
@@ -27,10 +31,25 @@ function Homepage() {
         <div className={styles.TripList}>
           <h1 style={{ textAlign: 'center' }}>Trips</h1>
           <TripList />
+
+          {trip ? (
+            <Slider
+              step={1}
+              stops={trip.stops}
+              onChangeSlider={setSliderPosition}
+              setCenter={setCenter}
+            />
+          ) : null}
+
           <LogoutButton />
         </div>
         <div className={styles.MapVisualizer}>
-          <MapVisualizer />
+          <MapVisualizer
+            center={center}
+            setCenter={setCenter}
+            sliderPosition={sliderPosition}
+            setSliderPosition={setSliderPosition}
+          />
         </div>
       </div>
     </ResourceContext.Provider>
